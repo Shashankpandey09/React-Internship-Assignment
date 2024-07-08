754343b9454959b0ef61d0192db9380853ad3798
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-07-08 04:19:07 -0800

    added use navigate in SecondPage.tsx

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -2,15 +2,16 @@ import React, { useEffect } from 'react';
 import { Container, Typography } from '@mui/material';
 import PostTable from './PostTable'; 
 import DepartmentList from './DepartmentList';
+import { useNavigate } from 'react-router-dom';
 
 const SecondPage: React.FC = () => {
-  
+   const navigate=useNavigate();
     useEffect(() => {
         // Check if user details are in localStorage
         const userDetails = localStorage.getItem('userDetails');
         if (!userDetails) {
           // Redirect back to the first page if details are not found
-          window.location.href = '/';
+          navigate('/');
         }
       }, []);
   return (
